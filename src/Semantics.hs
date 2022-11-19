62af3ea8ee008d4d9e1f9f25075f04080093d98b
ajw <mj.wesolowski@student.uw.edu.pl>
2022-11-19 20:28:22 +0100

    Type rework'

--- src/Semantics.hs
+++ src/Semantics.hs
@@ -28,86 +28,86 @@ type Context a = StateT Env (Writer [String]) a
 failure :: Show a => a -> Context ()
 failure x = tell ["Undefined case: " ++ show x]    
 
-transIdent :: Latte.Abs.Ident -> Context ()
+transIdent :: Ident -> Context ()
 transIdent x = case x of
-  Latte.Abs.Ident string -> failure x
+  Ident string -> failure x
 
 transProgram :: Program -> Context ()
 transProgram x = case x of
-  Latte.Abs.Program _ topdefs -> failure x
+  Program _ topdefs -> failure x
 
 transTopDef :: TopDef -> Context ()
 transTopDef x = case x of
-  Latte.Abs.FnDef _ type_ ident args block -> failure x
+  FnDef _ type_ ident args block -> failure x
 
-transArg :: Show a => Latte.Abs.Arg' a -> Context ()
+transArg ::Arg -> Context ()
 transArg x = case x of
-  Latte.Abs.Arg _ type_ ident -> failure x
+  Arg _ type_ ident -> failure x
 
-transBlock :: Show a => Latte.Abs.Block' a -> Context ()
+transBlock ::Block -> Context ()
 transBlock x = case x of
-  Latte.Abs.Block _ stmts -> failure x
+  Block _ stmts -> failure x
 
-transStmt :: Show a => Latte.Abs.Stmt' a -> Context ()
+transStmt ::Stmt -> Context ()
 transStmt x = case x of
-  Latte.Abs.Empty _ -> failure x
-  Latte.Abs.BStmt _ block -> failure x
-  Latte.Abs.Decl _ type_ items -> failure x
-  Latte.Abs.Ass _ ident expr -> failure x
-  Latte.Abs.Incr _ ident -> failure x
-  Latte.Abs.Decr _ ident -> failure x
-  Latte.Abs.Ret _ expr -> failure x
-  Latte.Abs.VRet _ -> failure x
-  Latte.Abs.Cond _ expr stmt -> failure x
-  Latte.Abs.CondElse _ expr stmt1 stmt2 -> failure x
-  Latte.Abs.While _ expr stmt -> failure x
-  Latte.Abs.SExp _ expr -> failure x
+  Empty _ -> failure x
+  BStmt _ block -> failure x
+  Decl _ type_ items -> failure x
+  Ass _ ident expr -> failure x
+  Incr _ ident -> failure x
+  Decr _ ident -> failure x
+  Ret _ expr -> failure x
+  VRet _ -> failure x
+  Cond _ expr stmt -> failure x
+  CondElse _ expr stmt1 stmt2 -> failure x
+  While _ expr stmt -> failure x
+  SExp _ expr -> failure x
 
-transItem :: Show a => Latte.Abs.Item' a -> Context ()
+transItem ::Item -> Context ()
 transItem x = case x of
-  Latte.Abs.NoInit _ ident -> failure x
-  Latte.Abs.Init _ ident expr -> failure x
+  NoInit _ ident -> failure x
+  Init _ ident expr -> failure x
 
-transType :: Show a => Latte.Abs.Type' a -> Context ()
+transType ::Type -> Context ()
 transType x = case x of
-  Latte.Abs.Int _ -> failure x
-  Latte.Abs.Str _ -> failure x
-  Latte.Abs.Bool _ -> failure x
-  Latte.Abs.Void _ -> failure x
-  Latte.Abs.Fun _ type_ types -> failure x
+  Int _ -> failure x
+  Str _ -> failure x
+  Bool _ -> failure x
+  Void _ -> failure x
+  Fun _ type_ types -> failure x
 
-transExpr :: Show a => Latte.Abs.Expr' a -> Context ()
+transExpr ::Expr -> Context ()
 transExpr x = case x of
-  Latte.Abs.EVar _ ident -> failure x
-  Latte.Abs.ELitInt _ integer -> failure x
-  Latte.Abs.ELitTrue _ -> failure x
-  Latte.Abs.ELitFalse _ -> failure x
-  Latte.Abs.EApp _ ident exprs -> failure x
-  Latte.Abs.EString _ string -> failure x
-  Latte.Abs.Neg _ expr -> failure x
-  Latte.Abs.Not _ expr -> failure x
-  Latte.Abs.EMul _ expr1 mulop expr2 -> failure x
-  Latte.Abs.EAdd _ expr1 addop expr2 -> failure x
-  Latte.Abs.ERel _ expr1 relop expr2 -> failure x
-  Latte.Abs.EAnd _ expr1 expr2 -> failure x
-  Latte.Abs.EOr _ expr1 expr2 -> failure x
+  EVar _ ident -> failure x
+  ELitInt _ integer -> failure x
+  ELitTrue _ -> failure x
+  ELitFalse _ -> failure x
+  EApp _ ident exprs -> failure x
+  EString _ string -> failure x
+  Neg _ expr -> failure x
+  Not _ expr -> failure x
+  EMul _ expr1 mulop expr2 -> failure x
+  EAdd _ expr1 addop expr2 -> failure x
+  ERel _ expr1 relop expr2 -> failure x
+  EAnd _ expr1 expr2 -> failure x
+  EOr _ expr1 expr2 -> failure x
 
-transAddOp :: Show a => Latte.Abs.AddOp' a -> Context ()
+transAddOp ::AddOp -> Context ()
 transAddOp x = case x of
-  Latte.Abs.Plus _ -> failure x
-  Latte.Abs.Minus _ -> failure x
+  Plus _ -> failure x
+  Minus _ -> failure x
 
-transMulOp :: Show a => Latte.Abs.MulOp' a -> Context ()
+transMulOp ::MulOp -> Context ()
 transMulOp x = case x of
-  Latte.Abs.Times _ -> failure x
-  Latte.Abs.Div _ -> failure x
-  Latte.Abs.Mod _ -> failure x
+  Times _ -> failure x
+  Div _ -> failure x
+  Mod _ -> failure x
 
-transRelOp :: Show a => Latte.Abs.RelOp' a -> Context ()
+transRelOp ::RelOp -> Context ()
 transRelOp x = case x of
-  Latte.Abs.LTH _ -> failure x
-  Latte.Abs.LE _ -> failure x
-  Latte.Abs.GTH _ -> failure x
-  Latte.Abs.GE _ -> failure x
-  Latte.Abs.EQU _ -> failure x
-  Latte.Abs.NE _ -> failure x
+  LTH _ -> failure x
+  LE _ -> failure x
+  GTH _ -> failure x
+  GE _ -> failure x
+  EQU _ -> failure x
+  NE _ -> failure x
