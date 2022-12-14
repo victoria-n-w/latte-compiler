a5156067f2e43e5b46871134dbd0c26f9800315c
ajw <mj.wesolowski@student.uw.edu.pl>
2022-12-14 22:24:29 +0100

    Trans expressions

--- src/Translate.hs
+++ src/Translate.hs
@@ -7,7 +7,10 @@ import Latte.ErrM
 
 -- module which translates code to internal representation
 
-type Arg = Int
+type Loc = Int
+
+data Arg = Var Loc | Const Integer | None
+  deriving (Show)
 
 data Op
   = Add
@@ -50,7 +53,7 @@ data Quadruple = Quadruple
 -- - map of variables to their addresses
 -- - result writer, storing a list of quadruples
 -- with internal monad error type
-type Context = RWST Translate.Arg [Quadruple] (Data.Map String Translate.Arg) Err
+type Context = RWST Loc [Quadruple] (Data.Map String Loc) Err
 
 translate :: Program -> Err [Quadruple]
 translate p = do
@@ -61,17 +64,8 @@ transProgram :: Latte.Abs.Program -> Context ()
 transProgram (Program loc topDefs) =
   mapM_ transTopDef topDefs
 
-getFreeArg :: Context Translate.Arg
-getFreeArg = do
-  freeVar <- ask
-  local (+ 1) $ return freeVar -- TODO check if it works
-
 failure :: Show a => a -> Context ()
 failure x = fail $ "Undefined case: " ++ show x
-
-transIdent :: Latte.Abs.Ident -> Context ()
-transIdent x = case x of
-  Latte.Abs.Ident string -> failure x
 
 transTopDef :: Latte.Abs.TopDef -> Context ()
 transTopDef x = case x of
@@ -80,11 +74,9 @@ transTopDef x = case x of
     transBlock block
 
 transArg :: Latte.Abs.Arg -> Context ()
-transArg (Latte.Abs.Arg _ _ (Ident ident)) = do
-  -- put argument to context
-  env <- get
-  freeArg <- getFreeArg
-  put $ Data.insert ident freeArg env
+transArg (Latte.Abs.Arg _ _ ident) = do
+  newVar ident
+  return ()
 
 transBlock :: Latte.Abs.Block -> Context ()
 transBlock (Latte.Abs.Block _ stmts) =
@@ -96,61 +88,101 @@ transStmt x = case x of
   BStmt _ block -> transBlock block
   Decl _ type_ items -> mapM_ transItem items
   Ass _ ident expr -> do
-    transExpr expr
-  Incr _ ident -> failure x
-  Decr _ ident -> failure x
-  Ret _ expr -> failure x
-  VRet _ -> failure x
+    res <- transExpr expr
+    loc <- newVar ident
+    tell [Quadruple Assign res None loc]
+  Incr _ ident -> do
+    oldLoc <- getVar ident
+    newLoc <- newVar ident
+    tell [Quadruple Add oldLoc (Const 1) newLoc]
+  Decr _ ident -> do
+    oldLoc <- getVar ident
+    newLoc <- newVar ident
+    tell [Quadruple Sub oldLoc (Const 1) newLoc]
+  Ret _ expr -> do
+    res <- transExpr expr
+    tell [Quadruple Return res None None]
+  VRet _ -> do
+    tell [Quadruple ReturnVoid None None None]
   Cond _ expr stmt -> failure x
   CondElse _ expr stmt1 stmt2 -> failure x
   While _ expr stmt -> failure x
-  SExp _ expr -> failure x
+  SExp _ expr -> do
+    transExpr expr
+    return ()
 
 transItem :: Latte.Abs.Item -> Context ()
 transItem x = case x of
-  Latte.Abs.NoInit _ (Ident ident) -> failure x
+  Latte.Abs.NoInit _ (Ident ident) -> return ()
   Latte.Abs.Init _ (Ident ident) expr -> failure x
 
-transType :: Latte.Abs.Type -> Context ()
-transType x = case x of
-  Latte.Abs.Int _ -> failure x
-  Latte.Abs.Str _ -> failure x
-  Latte.Abs.Bool _ -> failure x
-  Latte.Abs.Void _ -> failure x
-  Latte.Abs.Fun _ type_ types -> failure x
+newVar :: Ident -> Context Translate.Arg
+newVar (Ident ident) = do
+  env <- get
+  freeLoc <- getFreeLoc
+  put $ Data.insert ident freeLoc env
+  return $ Var freeLoc
 
-transExpr :: Latte.Abs.Expr -> Context ()
+getFreeLoc :: Context Loc
+getFreeLoc = do
+  freeVar <- ask
+  local (+ 1) $ return freeVar -- TODO check if it works
+
+getVar :: Ident -> Context Translate.Arg
+getVar (Ident ident) = do
+  env <- get
+  case Data.lookup ident env of
+    Just loc -> return $ Var loc
+    Nothing -> fail $ "Variable " ++ ident ++ " not found"
+
+transExpr :: Latte.Abs.Expr -> Context Translate.Arg
 transExpr x = case x of
-  Latte.Abs.EVar _ ident -> failure x
-  Latte.Abs.ELitInt _ integer -> failure x
-  Latte.Abs.ELitTrue _ -> failure x
-  Latte.Abs.ELitFalse _ -> failure x
-  Latte.Abs.EApp _ ident exprs -> failure x
-  Latte.Abs.EString _ string -> failure x
-  Latte.Abs.Neg _ expr -> failure x
-  Latte.Abs.Not _ expr -> failure x
-  Latte.Abs.EMul _ expr1 mulop expr2 -> failure x
-  Latte.Abs.EAdd _ expr1 addop expr2 -> failure x
-  Latte.Abs.ERel _ expr1 relop expr2 -> failure x
-  Latte.Abs.EAnd _ expr1 expr2 -> failure x
-  Latte.Abs.EOr _ expr1 expr2 -> failure x
+  EVar _ ident -> getVar ident
+  ELitInt _ integer -> return $ Const integer
+  ELitTrue _ -> failExp x
+  ELitFalse _ -> failExp x
+  EApp _ ident exprs -> failExp x
+  EString _ string -> failExp x
+  Latte.Abs.Neg _ expr -> failExp x
+  Latte.Abs.Not _ expr -> failExp x
+  EMul _ expr1 mulop expr2 ->
+    transBinOp expr1 expr2 (transMulOp mulop)
+  EAdd _ expr1 addop expr2 ->
+    transBinOp expr1 expr2 (transAddOp addop)
+  ERel _ expr1 relop expr2 ->
+    transBinOp expr1 expr2 (transRelOp relop)
+  EAnd _ expr1 expr2 ->
+    transBinOp expr1 expr2 And
+  EOr _ expr1 expr2 ->
+    transBinOp expr1 expr2 Or
 
-transAddOp :: Latte.Abs.AddOp -> Context ()
+transBinOp :: Expr -> Expr -> Op -> Context Translate.Arg
+transBinOp expr1 expr2 op = do
+  res1 <- transExpr expr1
+  res2 <- transExpr expr2
+  loc <- getFreeLoc
+  tell [Quadruple op res1 res2 (Var loc)]
+  return (Var loc)
+
+failExp :: Show a => a -> Context Translate.Arg
+failExp x = fail $ "Undefined case: " ++ show x
+
+transAddOp :: Latte.Abs.AddOp -> Op
 transAddOp x = case x of
-  Latte.Abs.Plus _ -> failure x
-  Latte.Abs.Minus _ -> failure x
+  Latte.Abs.Plus _ -> Add
+  Latte.Abs.Minus _ -> Sub
 
-transMulOp :: Latte.Abs.MulOp -> Context ()
+transMulOp :: Latte.Abs.MulOp -> Op
 transMulOp x = case x of
-  Latte.Abs.Times _ -> failure x
-  Latte.Abs.Div _ -> failure x
-  Latte.Abs.Mod _ -> failure x
+  Latte.Abs.Times _ -> Mul
+  Latte.Abs.Div _ -> Translate.Div
+  Latte.Abs.Mod _ -> Translate.Mod
 
-transRelOp :: Latte.Abs.RelOp -> Context ()
+transRelOp :: Latte.Abs.RelOp -> Op
 transRelOp x = case x of
-  Latte.Abs.LTH _ -> failure x
-  Latte.Abs.LE _ -> failure x
-  Latte.Abs.GTH _ -> failure x
-  Latte.Abs.GE _ -> failure x
-  Latte.Abs.EQU _ -> failure x
-  Latte.Abs.NE _ -> failure x
+  Latte.Abs.LTH _ -> Lt
+  Latte.Abs.LE _ -> Le
+  Latte.Abs.GTH _ -> Gt
+  Latte.Abs.GE _ -> Ge
+  Latte.Abs.EQU _ -> Eq
+  Latte.Abs.NE _ -> Neq
