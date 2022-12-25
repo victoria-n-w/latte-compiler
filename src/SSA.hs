400eec6908c4162fb926e049814a2a01f343eab7
ajw <mj.wesolowski@student.uw.edu.pl>
2022-12-25 16:06:19 +0100

    Add prievious blocks' labels

--- src/SSA.hs
+++ src/SSA.hs
@@ -2,7 +2,8 @@ module SSA where
 
 import Block (Block (..), BlockMap)
 import Control.Monad.State
+import Quadruples (LabelName)
 
 -- | Transforms a map of blocks into a map of SSA blocks.
 transpose :: BlockMap -> BlockMap
-transpose = undefined
+transpose b = b
