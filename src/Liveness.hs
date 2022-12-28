d7b3fea39735953dea1e88384d0d5b25ae541d38
ajw <mj.wesolowski@student.uw.edu.pl>
2022-12-28 18:42:13 +0100

    Create LBlock structure

--- src/Liveness.hs
+++ src/Liveness.hs
@@ -1,8 +1,11 @@
 module Liveness where
 
-import Data.Set
+import Data.List (intercalate)
+import Data.Map
+import Data.Set (Set)
 import Quadruples (Arg (..), LabelName, Loc, Op (..), Quadruple (..))
 import SSA
+import Text.Printf (printf)
 
 data Liveness = Liveness
   { inLive :: Set Loc,
@@ -10,3 +13,50 @@ data Liveness = Liveness
     kill :: Set Loc,
     use :: Set Loc
   }
+
+instance Show Liveness where
+  show :: Liveness -> String
+  show (Liveness inLive out kill use) =
+    printf "in: %s, out: %s, kill: %s, use: %s" (show inLive) (show out) (show kill) (show use)
+
+data LBlock = LBlock
+  { label :: LabelName,
+    block :: [(Quadruple, Liveness)],
+    phiMap :: PhiMap,
+    next :: [LabelName],
+    previous :: [LabelName],
+    liveness :: Liveness
+  }
+
+instance Show LBlock where
+  show :: LBlock -> String
+  show (LBlock label block phiMap next prvs liveness) =
+    "---\n"
+      ++ label
+      ++ ":\n"
+      ++ "previous:\n"
+      -- add indentation
+      ++ unlines (Prelude.map ("\t" ++) prvs)
+      ++ "phi:\n"
+      ++ unlines
+        ( Prelude.map
+            ( \(loc, phi) ->
+                printf
+                  "\tphi(%d) = %s"
+                  loc
+                  $ intercalate
+                    ","
+                    ( Prelude.map
+                        (\(label, loc) -> printf "%s:%s" label (show loc))
+                        (Data.Map.toList phi)
+                    )
+            )
+            (Data.Map.toList phiMap)
+        )
+      ++ "block:\n"
+      ++ unlines (Prelude.map (\(q, l) -> show q ++ "\t" ++ show l) block)
+      ++ "next:\n"
+      ++ unlines (Prelude.map ("\t" ++) next)
+      ++ "liveness:\n"
+      ++ "\t"
+      ++ show liveness
