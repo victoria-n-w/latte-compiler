61ac90c243206955a0f431919b69f563f9ea4623
ajw <mj.wesolowski@student.uw.edu.pl>
2022-12-31 01:03:27 +0100

    Operains

--- src/Assembler.hs
+++ src/Assembler.hs
@@ -4,6 +4,7 @@ import Control.Monad.RWS
 import Data.List (intercalate)
 import Data.Map qualified as Map
 import Data.Maybe
+import Latte.ErrM
 import Liveness (LBlock (..), LBlockMap, LiveQuadruple, LiveVars)
 import Quadruples (Arg (..), LabelName, Loc, Op (..), Quadruple (..))
 import SSA (Phi, PhiMap)
@@ -33,7 +34,12 @@ generateBlocks =
 generateBlock :: LBlock -> Context ()
 generateBlock (LBlock _ block phiMap _ _ inLive outLive) = do
   parsePhi phiMap
-  mapM_ generateQuadruple block
+  mapM_
+    ( \q -> do
+        generateQuadruple q
+        killDeadVars q
+    )
+    block
 
 type Context = RWS () [String] (RegisterMap, Memory, VarMap)
 
@@ -61,15 +67,72 @@ data VarLoc = Reg RegisterName | InMem Loc
 
 generateQuadruple :: LiveQuadruple -> Context ()
 generateQuadruple (quad, liveVars) = do
-  case Quadruples.res quad of
-    Var loc -> do
-      assignVariable loc
-      return ()
-    _ -> return ()
   case quad of
-    (Quadruple op arg1 arg2 res) ->
-      do
-        tell [show quad]
+    Quadruple Add arg1 arg2 (Var res) -> do
+      generateOp liveVars arg1 arg2 res "add"
+    Quadruple (Label label) _ _ _ -> do
+      tell [printf "%s:" label]
+    -- don't process jumps yet
+    Quadruple Jump arg _ _ -> return ()
+    Quadruple JumpIf cnd label1 label2 -> return ()
+    Quadruple Return arg _ _ -> do
+      case arg of
+        Var loc -> do
+          varLoc <- getVariableLocation loc
+          case varLoc of
+            Reg regName -> do
+              tell [printf "mov rax, %s" (show regName)]
+            InMem memLoc -> do
+              tell [printf "mov rax, [rbp - %d]" (memLoc * 8)]
+        _ -> do
+          tell [printf "mov rax, %s" (show arg)]
+      tell ["leave", "ret"]
+    Quadruple ReturnVoid _ _ _ -> do
+      tell ["leave", "ret"]
+    Quadruple Nop _ _ _ -> return ()
+    Quadruple op arg1 arg2 res -> do
+      tell [printf "%s %s, %s, %s" (show op) (show arg1) (show arg2) (show res)]
+
+generateOp :: LiveVars -> Arg -> Arg -> Loc -> String -> Context ()
+generateOp liveVars arg1 arg2 res op = do
+  opReg <- case arg1 of
+    Var loc -> do
+      varLoc <- getVariableLocation loc
+      case varLoc of
+        Reg regName -> do
+          return regName
+        InMem memLoc -> do
+          opReg' <- forceGetRegister
+          tell [printf "mov %s, [rbp - %d]" (show opReg') (memLoc * 8)]
+          return opReg'
+    _ -> do
+      opReg' <- forceGetRegister
+      tell [printf "mov %s, %s" (show opReg') (show arg1)]
+      return opReg'
+  -- if arg1 is live after this instruction, we need to save it
+  case arg1 of
+    Var loc -> do
+      when (loc `elem` liveVars) $ do
+        newVarReg <- forceGetRegister
+        tell [printf "mov %s, %s" (show newVarReg) (show opReg)]
+        assignToRegister newVarReg loc
+    _ -> return ()
+  assignToRegister opReg res
+  -- emit the operation
+  case arg2 of
+    Var loc -> do
+      varLoc <- getVariableLocation loc
+      case varLoc of
+        Reg regName -> do
+          tell [printf "%s %s, %s" op (show opReg) (show regName)]
+        InMem memLoc -> do
+          tell [printf "%s %s, [rbp - %d]" op (show opReg) (memLoc * 8)]
+    _ -> do
+      tell [printf "%s %s, %s" op (show opReg) (show arg2)]
+
+killDeadVars :: LiveQuadruple -> Context ()
+-- TODO
+killDeadVars _ = return ()
 
 parsePhi :: PhiMap -> Context ()
 parsePhi phiMap = do
@@ -100,6 +163,13 @@ getVariableLocation loc = do
   (_, _, varMap) <- get
   return $ varMap Map.! loc
 
+forceGetRegister :: Context RegisterName
+-- TODO this is not correct, we need to spill a register
+forceGetRegister = do
+  (regMap, _, _) <- get
+  let freeRegs = Map.filter (== Empty) regMap
+  return $ fst $ Map.findMin freeRegs
+
 -- | Returns the freed register name and variable that was assigned to it
 -- Precondition: there is a register to spill
 getFreeRegister :: Context (Maybe RegisterName)
@@ -108,11 +178,13 @@ getFreeRegister = do
   let freeRegs = Map.filter (== Empty) regMap
   return $ if Map.null freeRegs then Nothing else Just $ fst $ Map.findMin freeRegs
 
+-- | Performs operation on the State monad
 assignToRegister :: RegisterName -> Loc -> Context ()
 assignToRegister regName loc = do
   (regMap, mmry, varMap) <- get
   put (Map.insert regName (Full loc) regMap, mmry, Map.insert loc (Reg regName) varMap)
 
+-- | Performs operation on the State monad
 assignToMemory :: Loc -> Context Int
 assignToMemory loc = do
   (regMap, Memory nextLoc memMap, varMap) <- get
