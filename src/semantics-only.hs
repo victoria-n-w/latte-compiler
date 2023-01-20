d9467fb2516c5741f136461bb56ac624118d3b5b
ajw <mj.wesolowski@student.uw.edu.pl>
2023-01-20 13:05:15 +0100

    to ammend

---
+++ src/semantics-only.hs
@@ -0,0 +1,31 @@
+module Main where
+
+import Latte.ErrM
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import System.IO
+
+process :: String -> Err String
+process source = do
+  program <- pProgram $ myLexer source
+  case Semantics.verify program of
+    Semantics.Error err ->
+      Bad $
+        intercalate "\n" $
+          map show err
+    Semantics.Ok ->
+      translate program
+
+main :: IO ()
+main = do
+  -- open the file specficied by the first command line argument
+  -- and read its contents into a string
+  [fileName] <- getArgs
+  handle <- openFile fileName ReadMode
+  source <- hGetContents handle
+  case process source of
+    Latte.ErrM.Ok res ->
+      putStrLn res
+    Bad msg -> do
+      hPutStrLn stderr msg
+      exitFailure
